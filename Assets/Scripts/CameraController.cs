411f0234f61091759985817f28fec3650240cf8c
Alexander <vsany@meta.ua>
2021-09-10 08:08:24 +0300

    [+] Turn on the right\left by pressing A or D even more > or < buttons

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,11 @@ using UnityEngine.Serialization;
 public class CameraController : MonoBehaviour
 {
     public GameObject carModel;
+    private readonly Vector3 _offset = new Vector3(0, 7.24f, -10.63f);
 
 
-    void Update()
+    void LateUpdate()
     {
-        transform.position = carModel.transform.position;
+        transform.position = carModel.transform.position + _offset;
     }
 }
--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private float _speed = 15;
+    private readonly float _speed = 16;
+    public float turnSpeed = 5;
+    public float turnOn;
 
 
     void Update()
     {
+        turnOn = Input.GetAxis("Horizontal");
+
         transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * turnOn);
     }
 }
